4170a3bf48004e3a3cb05d80a7afd7bf71c66185
VamsiPusapati <vamsi.pusapati2014@gmail.com>
2016-02-12 18:34:07 -0400

    updated the page links and app.css

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,18 +7,11 @@
 
 	var aboutMe;
 	aboutMe = document.getElementById("aboutMe");
-	document.getElementById("aboutMe").innerHTML = "I Create & Design the Websites by the process of collecting ideas,and implementing them,guided by certain principles for a specific purpose.Web design is a similar process of creation,with the intention to create the Websites which usually includes a concerted focus on performance, aesthetics, user experience and interaction, social media engagement and web presence optimization for presenting the content on electronic web pages.";
+	document.getElementById("aboutMe").innerHTML = "I Create & Design the Websites by the process of collecting ideas,and implementing them,guided by certain principles for a specific purpose.Web design is a similar process of creation,with the intention to create the Websites which usually includes a concerted focus on performance, aesthetics, user experience and interaction, social media engagement and web presence optimization for presenting the content on electronic web pages."
 	
 		
-	var projectInfo;
-	projectInfo = document.getElementById("projectInfo");
-	document.getElementById("projectInfo").innerHTML = "Projects thklsenm,smgip/KGN.S.G.SM.MN.FNKBL/JIFk/.snfb.s.mfbmmfbmfnbfnb.n/snb.";
-
+	var info;
+	info = document.getElementById("info");
+	document.getElementById("info").innerHTML ="vamsi"
 	
-	
-})();
-
-(function (){
-	console.log( "App Started");
-
 })();
