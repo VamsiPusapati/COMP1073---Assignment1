c90f330430217477313548665544fd93ebd65217
VamsiPusapati <vamsi.pusapati2014@gmail.com>
2016-02-11 23:01:23 -0400

    Updated css

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,5 +8,17 @@
 	var aboutMe;
 	aboutMe = document.getElementById("aboutMe");
 	document.getElementById("aboutMe").innerHTML = "I Create & Design the Websites by the process of collecting ideas,and implementing them,guided by certain principles for a specific purpose.Web design is a similar process of creation,with the intention to create the Websites which usually includes a concerted focus on performance, aesthetics, user experience and interaction, social media engagement and web presence optimization for presenting the content on electronic web pages.";
-						
+	
+		
+	var projectInfo;
+	projectInfo = document.getElementById("projectInfo");
+	document.getElementById("projectInfo").innerHTML = "Projects thklsenm,smgip/KGN.S.G.SM.MN.FNKBL/JIFk/.snfb.s.mfbmmfbmfnbfnb.n/snb.";
+
+	
+	
+})();
+
+(function (){
+	console.log( "App Started");
+
 })();
