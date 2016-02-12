030203be55381f75fdae78e2af941d79ec901c0d
VamsiPusapati <vamsi.pusapati2014@gmail.com>
2016-02-11 21:35:09 -0400

    updated contact information

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,12 @@
 "use strict";
 
 // setup your IIFE (Immediately Invoked Function Expression)
-(function(){
-	
+(function () {
+
 	console.log("App Started...");
-	
+
+	var aboutMe;
+	aboutMe = document.getElementById("aboutMe");
+	document.getElementById("aboutMe").innerHTML = "I Create & Design the Websites by the process of collecting ideas,and implementing them,guided by certain principles for a specific purpose.Web design is a similar process of creation,with the intention to create the Websites which usually includes a concerted focus on performance, aesthetics, user experience and interaction, social media engagement and web presence optimization for presenting the content on electronic web pages.";
+						
 })();
